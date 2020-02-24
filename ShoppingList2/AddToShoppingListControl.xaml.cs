05ebf889c6f77808fc1035628fd1073df2c1eb0d
May Phyo <phyo@ksu.edu>
2020-02-24 15:32:57 -0600

    Done

---
+++ ShoppingList2/AddToShoppingListControl.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList2
+{
+    /// <summary>
+    /// Interaction logic for AddToShoppingListControl.xaml
+    /// </summary>
+    public partial class AddToShoppingListControl : UserControl
+    {
+        public AddToShoppingListControl()
+        {
+            InitializeComponent();
+            AddButton.Click += OnAddButtonClicked;
+        }
+
+        public void OnAddButtonClicked(object sender, RoutedEventArgs e)
+        {
+
+            // var data = DataContext as ShoppingListData; 
+            if (DataContext is ShoppingListData data)
+            {
+                data.Add(ItemToAdd.Text);
+                ItemToAdd.Text = "";
+            }
+        }
+    }
+}
---
+++ ShoppingList2/ShoppingListData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ShoppingList2
+{
+    public class ShoppingListData : INotifyPropertyChanged
+    {
+        List<string> items = new List<string>();
+        public IEnumerable<string> Items => items.ToArray();
+
+        public event PropertyChangedEventHandler PropertyChanged; 
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+        
+        
+    }
+}
